4e882f05dbe756776a514e4194ebc2aa6c7b2257
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2025-03-11 11:30:44 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/todos").then((res) => setTodos(res.data));
+  }, []);
+
+  const addTodo = async () => {
+    if (!task) return;
+    const res = await axios.post("http://localhost:5000/todos", { task });
+    setTodos([...todos, res.data]);
+    setTask("");
+  };
+
+  const toggleComplete = async (id, completed) => {
+    await axios.put(`http://localhost:5000/todos/${id}`, {
+      completed: !completed,
+    });
+    setTodos(
+      todos.map((todo) =>
+        todo._id === id ? { ...todo, completed: !completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`http://localhost:5000/todos/${id}`);
+    setTodos(todos.filter((todo) => todo._id !== id));
+  };
+
+  return (
+    <div className="container">
+      <h2>To-Do App</h2>
+      <input
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="New Task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            <span
+              style={{ textDecoration: todo.completed ? "line-through" : "" }}
+            >
+              {todo.task}
+            </span>
+            <button onClick={() => toggleComplete(todo._id, todo.completed)}>
+              {todo.completed ? "Undo" : "Complete"}
+            </button>
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
